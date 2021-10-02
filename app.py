d461bb57643408c7a214e5b4e5c9f78d1b5c23eb
rushikesh2210 <70270584+rushikesh2210@users.noreply.github.com>
2021-10-02 15:14:31 -0700

    Retrieve functionality working

--- app.py
+++ app.py
@@ -65,20 +65,22 @@ def index():
 
 @app.route("/retrieve", methods=["GET", "POST"])
 def retrieve():
-    if request.method == "GET":
+    if request.method == "POST":
 
         # check whether all fields filled
         if not request.form.get("rurl"):
             "please fill out all required fields"
-
-        # generate a code using one of the helper function
-        codes = c.execute("SELECT * FROM urls WHERE code=rurl").fetchall()
+    
         
+        code_1 = request.form.get("rurl")
+
+        # check whether the code created is valid
+        codes = c.execute("SELECT original_url FROM urls WHERE auto_code=:code_1 OR code=:code_1", {"code_1" : code_1 }).fetchall()
 
         # render different template based on wheter user logged in or not
         if session.get("user_id"):
-            return render_template("confirm.html", codes=codes, original_url=request.form.get("rurl"))
-        return render_template("success.html", codes=codes, old=request.form.get("rurl"))
+            return render_template("retrieve.html", value=codes)
+        return render_template("retrieve.html", value=codes)
 
     else:
         return render_template("index.html")
