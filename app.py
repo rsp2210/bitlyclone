e35e164f72dd14f16060961c7b616c061243e2dc
rushikesh2210 <70270584+rushikesh2210@users.noreply.github.com>
2021-10-04 12:03:07 -0700

    Modified templates

--- app.py
+++ app.py
@@ -26,59 +26,38 @@ BASE_URL = "http://rp-bitlyclone.herokuapp.com/url/"
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-
-        # check whether all fields filled 
         if not request.form.get("url"):
             "please fill out all required fields"
-
-        # generate a code using one of the helper function
         auto_code = random_str()
-
-        # check whether the code created is valid
         codes = c.execute("SELECT * FROM urls WHERE auto_code=:auto_code OR code=:auto_code", {"auto_code" : auto_code}).fetchall()
         while len(codes) != 0:
             auto_code = random_str()
             codes = c.execute("SELECT * FROM urls WHERE auto_code=:auto_code OR code=:auto_code", {"auto_code" : auto_code}).fetchall()
-
-        # get the date and timestamp
+       
         import time
         from datetime import date
-        # get today's date
+       
         today = date.today()
-        # mm/dd/yyyy
         date = today.strftime("%m/%d/%y")
-        # pure timestamp
         ts = time.gmtime()
-        # readable timestamp
         timestamp = time.strftime("%x %X", ts)
-
-        # INSERT into urls with all the information and commit to the database
         c.execute("INSERT INTO urls (original_url, auto_code, code, date, timestamp, user_id, click) VALUES (:o_url, :code, :code, :date, :time, :u_id, 0)", {"o_url": request.form.get("url"), "code": auto_code, "date": date, "time": timestamp, "u_id": session.get("user_id")})
         conn.commit()
 
-        # render different template based on wheter user logged in or not
         if session.get("user_id"):
             return render_template("confirm.html", BASE_URL=BASE_URL, code=auto_code, original_url=request.form.get("url"))
         return render_template("success.html", BASE_URL=BASE_URL, auto_code=auto_code, old=request.form.get("url"))
-
     else:
         return render_template("index.html")
 
 @app.route("/retrieve", methods=["GET", "POST"])
 def retrieve():
     if request.method == "POST":
-
-        # check whether all fields filled
         if not request.form.get("rurl"):
-            "please fill out all required fields"
-    
-        
+            "please fill out all required fields"    
         code_1 = request.form.get("rurl")
         obj = code_1.rsplit('/', 1)[-1]
-        # check whether the code created is valid
         codes = c.execute("SELECT * FROM urls WHERE auto_code=:obj OR code=:obj", {"obj" : obj }).fetchall()
-
-        # render different template based on wheter user logged in or not
         if session.get("user_id"):
             return render_template("retrieve.html", value=codes,BASE_URL=BASE_URL)
         return render_template("retrieve.html", value=codes,BASE_URL=BASE_URL)
@@ -89,29 +68,20 @@ def retrieve():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        # check if the form is valid
-
             if not request.form.get("email") or not request.form.get("password") or not request.form.get("confirmation"):
                 return "please fill out all fields"
 
             if request.form.get("password") != request.form.get("confirmation"):
                 return "password confirmation doesn't match password"
-
-            # check if email exist in the database
             exist = c.execute("SELECT * FROM users WHERE email=:email", {"email": request.form.get("email")}).fetchall()
-
             if len(exist) != 0:
-                return "user already registered"
-
-            # hash the password
+                return render_template("alreadyregistered.html")
             pwhash = generate_password_hash(request.form.get("password"), method="pbkdf2:sha256", salt_length=8)
 
-            # insert the row
             c.execute("INSERT INTO users (email, password) VALUES (:email, :password)", {"email": request.form.get("email"), "password": pwhash})
             conn.commit()
 
-            # return success
-            return "registered successfully!"
+            return render_template("registered.html")
     else:
         return render_template("register.html")
 
@@ -120,27 +90,18 @@ def register():
 def login():
 
     if request.method == "POST":
-        # check the form is valid
         if not request.form.get("email") or not request.form.get("password"):
             return "please fill out all required fields"
 
-        # check if email exist in the database
         user = c.execute("SELECT * FROM users WHERE email=:email", {"email": request.form.get("email")}).fetchall()
 
         if len(user) != 1:
-            return "you didn't register"
-
-        # check the password is same to password hash
+            return render_template("didntregister.html")
         pwhash = user[0][2]
         if check_password_hash(pwhash, request.form.get("password")) == False:
-            return "wrong password"
-
-        # login the user using session
+            return render_template("wpswd.html")
         session["user_id"] = user[0][0]
-
-        # return success
-        return redirect("/dashboard")
-
+        return redirect("/")
     else:
         return render_template("login.html")
 
@@ -152,27 +113,21 @@ def logout():
 
 @app.route("/url/<string:code>")
 def url(code):
-
     result = c.execute("SELECT * FROM urls WHERE code=:code", {"code": code}).fetchall()
-
     if len(result) != 1:
         return "404"
 
     c.execute("UPDATE urls SET click = :c WHERE url_id=:id", {"c": int(result[0][7]) + 1, "id": result[0][0]})
     conn.commit()
-
     return redirect(result[0][1])
 
 
 @app.route("/update", methods=["GET", "POST"])
 @login_required
 def update():
-
     if request.method  == "POST":
-
         if not request.form.get("new"):
             return "please fill out ALL required fields"
-
         if not request.form.get("new").isalnum():
             return "You must provide ONLY alpha numeric value"
 
@@ -180,21 +135,15 @@ def update():
 
         if len(codes) != 0:
             return "code already exists"
-
         c.execute("UPDATE urls SET code=:new WHERE auto_code=:code OR code=:code", {"new": request.form.get("new"), "code": request.form.get("code")})
         conn.commit()
-
         return redirect("/dashboard")
-
     else:
         if not request.args.get("id"):
             return "please fill out all required fields"
-
         url = c.execute("SELECT * FROM urls WHERE url_id=:id", {"id": request.args.get("id")}).fetchall()
-
         if session.get("user_id") != url[0][6]:
             return "403"
-
         return render_template("confirm.html", BASE_URL=BASE_URL, code=url[0][3], original_url=url[0][1])
 
 
@@ -207,51 +156,33 @@ def dashboard():
 
 @app.route("/api", methods=["GET"])
 def api():
-    # Check is custom code available
     if request.args.get("custom") and not request.args.get("url"):
-
         url = c.execute("SELECT * FROM urls WHERE auto_code = :custom OR code = :custom", {"custom": request.args.get("custom")}).fetchall()
-
         if len(url) == 0:
             return jsonify(code=200, description="Your custom code is available as of now")
         return jsonify(code=400, error="Your custom code already exist")
-
     if not request.args.get("url"):
             return jsonify(code=400, error="Please fill out url parameter")
-
     if not validate_url(request.args.get("url")):
         return jsonify(code=400, error="Your URL is not valid")
-
     if not request.args.get("custom"):
-
         auto_code = random_str()
-
         codes = c.execute("SELECT * FROM urls WHERE auto_code=:auto_code OR code=:auto_code", {"auto_code" : auto_code}).fetchall()
-
         while len(codes) != 0:
             auto_code = random_str()
-
             codes = c.execute("SELECT * FROM urls WHERE auto_code=:auto_code OR code=:auto_code", {"auto_code" : auto_code}).fetchall()
-
         c.execute("INSERT INTO urls (original_url, auto_code, code, click) VALUES (:o_url, :code, :code, 0)", {"o_url": request.args.get("url"), "code": auto_code})
-
         conn.commit()
-
         return jsonify(code=200, url=f"{BASE_URL}{auto_code}")
-
     if request.args.get("custom"):
-
         url = c.execute("SELECT * FROM urls WHERE auto_code = :custom OR code = :custom", {"custom": request.args.get("custom")}).fetchall()
-
         if len(url) != 0:
             return jsonify(code=400, error="code already exists")
 
         c.execute("INSERT INTO urls (original_url, auto_code, code, click) VALUES (:o_url, :code, :code, 0)", {"o_url": request.args.get("url"), "code": request.args.get("custom")})
-
         conn.commit()
 
         return jsonify(code=200, url=f"{BASE_URL}{request.args.get('custom')}")
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=1234, debug=True)
--- helpers.py
+++ helpers.py
@@ -1,6 +1,5 @@
 import random
 import string
-
 from functools import wraps
 from flask import request, redirect, url_for, session
 
@@ -9,7 +8,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
-            return redirect(url_for('login')) # , next=request.url
+            return redirect(url_for('login')) 
         return f(*args, **kwargs)
     return decorated_function
 
@@ -23,11 +22,11 @@ def random_str(digit=7):
 def validate_url(url):
     import re
     regex = re.compile(
-        r'^(?:http|ftp)s?://' # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-        r'localhost|' #localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-        r'(?::\d+)?' # optional port
+        r'^(?:http|ftp)s?://' 
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' 
+        r'localhost|' 
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' 
+        r'(?::\d+)?' 
         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
-    return (re.match(regex, url) is not None) # True
+    return (re.match(regex, url) is not None) 
